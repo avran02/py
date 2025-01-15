e7bc9cf4e675694cd766de99c5393c42f8bd1b66
andrey <avran2002@gmail.com>
2025-01-15 18:03:33 +0300

    a

--- wednesday/main.py
+++ wednesday/main.py
@@ -1,22 +1,40 @@
 import pygame
 from pygame.locals import *
+from random import randint
 
 HEIGHT = 600
 WIDTH = 800
 RUNNING = True
-BACKGROUND_COLOR = "#666666"
-HEAD_COLOR = "#e84adb"
+BACKGROUND_COLOR = '#666666'
+HEAD_COLOR = '#e84adb'
 HEAD_SIZE = 50
-DIRECTION = (2, 0)
-HEAD = Rect(WIDTH/2-HEAD_SIZE/2, HEIGHT/2-HEAD_SIZE/2, HEAD_SIZE, HEAD_SIZE)
+DIRECTION = (0, 0)
+HEAD = Rect(
+    WIDTH/2-HEAD_SIZE/2, # начальный X 
+    HEIGHT/2-HEAD_SIZE/2, # начальный Y
+    HEAD_SIZE, # ширина
+    HEAD_SIZE, # высота
+)
+FOOD_SIZE = 30
+FOOD_COLOR = '#36e485'
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+def get_random_coords():
+    x = randint(0, WIDTH-FOOD_SIZE)
+    y = randint(0, HEIGHT-FOOD_SIZE)
+    return x, y
+
+def spawn_food():
+    x, y = get_random_coords()
+    pygame.draw.circle(screen, FOOD_COLOR, (x, y), FOOD_SIZE)
+
 def handle_keys():
     global DIRECTION
     pressed_keys = pygame.key.get_pressed()
+    # arrows
     if pressed_keys[K_UP]:
         DIRECTION = (0, -2)
     if pressed_keys[K_DOWN]:
@@ -25,6 +43,7 @@ def handle_keys():
         DIRECTION = (-2, 0)
     if pressed_keys[K_RIGHT]:
         DIRECTION = (2, 0)
+    # wasd
     if pressed_keys[K_w]:
         DIRECTION = (0, -2)
     if pressed_keys[K_s]:
@@ -34,17 +53,7 @@ def handle_keys():
     if pressed_keys[K_d]:
         DIRECTION = (2, 0)
 
-while RUNNING:
-    # Обрабатываем события
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
-    # Ставим фон
-    screen.fill(BACKGROUND_COLOR)
-
-    # Куда рисовать, Какого цвета, что рисовать
-    pygame.draw.rect(screen, HEAD_COLOR, HEAD)
-
+def handle_wall_touch():
     if HEAD.left < 0:
         HEAD.move_ip(WIDTH, 0)
     if HEAD.right > WIDTH:
@@ -53,7 +62,20 @@ while RUNNING:
         HEAD.move_ip(0, -HEIGHT)
     if HEAD.top < 0:
         HEAD.move_ip(0, HEIGHT)
-    
+
+while RUNNING:
+    # Обрабатываем события
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
+    # Ставим фон
+    screen.fill(BACKGROUND_COLOR)
+    spawn_food()
+
+    # Куда рисовать, Какого цвета, что рисовать
+    pygame.draw.rect(screen, HEAD_COLOR, HEAD)
+
+    handle_wall_touch()
     handle_keys()
     HEAD.move_ip(DIRECTION)
 
@@ -63,6 +85,3 @@ while RUNNING:
     # Максимальный FPS
     clock.tick(60)
 pygame.quit()
-
-
-
