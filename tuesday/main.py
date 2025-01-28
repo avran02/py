f4bf0cdf2803bfcd2088ff5c17fed4868a73cba1
andrey <avran2002@gmail.com>
2025-01-28 17:27:34 +0300

    u

--- tuesday/main.py
+++ tuesday/main.py
@@ -8,6 +8,7 @@ WIDTH = 800
 HEIGHT = 600
 HEAD_SIZE = 20
 PLAY = True
+BACKGROUND_COLOR = "#333333"
 DIRECTION = (0,0)
 pygame.font.init()
 font = pygame.font.Font('font/GochiHand-Regular.ttf', 36)
@@ -25,24 +26,21 @@ HEAD = Rect(head_x, head_y, HEAD_SIZE, HEAD_SIZE) # (x, y, ширина, выс�
 TAIL_COLOR = "#2e4770"
 BODY = []
 
+FONT_COLOR = "#fa13ef75"
 FOOD_COLOR = "#36e485"
 FOOD_SIZE = 20
 FOOD_POS = get_random_coords(WIDTH, HEIGHT, HEAD_SIZE)
 FOOD = pygame.draw.circle(screen, FOOD_COLOR, FOOD_POS, FOOD_SIZE)    
 
 def render_score():
-    text = font.render("score: " + str(POINTS), True, (255, 255, 255, 0.5))
-    text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+    text_to_render = "score: " + str(POINTS)
+    text = font.render(text_to_render, True, FONT_COLOR)
+    text_rect = text.get_rect(center=(WIDTH/2, HEIGHT*0.2))
     screen.blit(text, text_rect)
 
 def spawn_tail():
-    if len(BODY) == 0:
-        new_tail_part = Rect(HEAD.x, HEAD.y, HEAD_SIZE, HEAD_SIZE)
-        BODY.append(new_tail_part)
-    else:
-        new_tail_part = Rect(BODY[-1].x, BODY[-1].y, HEAD_SIZE, HEAD_SIZE)
-        BODY.append(new_tail_part)
-        print(len(BODY))
+    new_tail_part = Rect(HEAD.x, HEAD.y, HEAD_SIZE, HEAD_SIZE)
+    BODY.append(new_tail_part)
 
 def handle_food_touch():
     global POINTS, FOOD_POS
@@ -50,14 +48,14 @@ def handle_food_touch():
 
     if FOOD.colliderect(HEAD):
         POINTS += 1
-        print("Вы съели", POINTS, "вкусняшек")
         FOOD_POS = get_random_coords(WIDTH, HEIGHT, HEAD_SIZE)
         FOOD.move_ip(FOOD_POS)
         spawn_tail()
 
 
-def handle_keys():
+def move(head):
     global DIRECTION
+    # Изменяем направление, при нажатии на клавиши
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[K_UP]:
         DIRECTION = (0, -SPEED)
@@ -67,8 +65,7 @@ def handle_keys():
         DIRECTION = (-SPEED, 0)
     if pressed_keys[K_RIGHT]:
         DIRECTION = (SPEED, 0)
-
-def handle_wall_touch(head):
+    # Телепортируем змейку в случае выхода за границы экрана
     if head.top < 0:
         head.move_ip(0, HEIGHT)
     if head.bottom > HEIGHT:
@@ -77,6 +74,19 @@ def handle_wall_touch(head):
         head.move_ip(WIDTH, 0)
     if head.right > WIDTH:
         head.move_ip(-WIDTH, 0)
+    # Двигаем змейку
+    if len(BODY) == 0:
+        head.move_ip(DIRECTION)
+        return
+    BODY[0].x = head.x
+    BODY[0].y = head.y
+    for i in range(len(BODY)-1, 0, -1):
+        BODY[i].x = BODY[i-1].x
+        BODY[i].y = BODY[i-1].y
+
+    head.move_ip(DIRECTION)
+
+
 
 while PLAY:
     # Обрабатываем события
@@ -84,11 +94,9 @@ while PLAY:
         if event.type == pygame.QUIT:
             PLAY = False
     
-    screen.fill("#333333") # Ставим фон
+    screen.fill(BACKGROUND_COLOR) # Ставим фон
+    move(HEAD)
     pygame.draw.rect(screen, head_color, HEAD) # Рисуем голову
-    HEAD.move_ip(DIRECTION)
-    handle_wall_touch(HEAD)
-    handle_keys()
     handle_food_touch()
     render_score()
 
